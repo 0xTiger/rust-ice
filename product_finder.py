9912f916794f6bcaddb9778420baeec296e29c9e
0xTiger <tgh.lizard@googlemail.com>
2023-08-30 20:39:21 +0100

    feat: improve product finder ergonomics

--- product_finder.py
+++ product_finder.py
@@ -1,23 +1,24 @@
-import json
 import re
 import time
 
-from sqlalchemy.exc import IntegrityError
-
+from sqlalchemy import text
 from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchWindowException
 
-from db import db_ctx, Product
+from db import db_ctx
+from scraper import bcolors
+
 
 url = 'https://groceries.asda.com/'
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
+browser = Chrome()
+browser.get(url)
 
+
+with db_ctx() as db:
+    existing_products = set(db.execute(text('SELECT url FROM product')).scalars())
 found_products = set()
 found_cats = set()
-browser = Chrome(options=chrome_options)
-browser.get(url)
 while True:
     try:
         soup = BeautifulSoup(browser.page_source, features="html.parser")
@@ -27,8 +28,18 @@ while True:
         found_cats |= {'https://groceries.asda.com/cat/' + x.group(1) 
             for x in re.finditer(r'/cat/([\-/a-zA-Z0-9]+)', str(soup))
         }
-        print(f'{len(found_cats)} {len(found_products)}', flush=True, end='\r')
-        time.sleep(0.5)
-    except KeyboardInterrupt:
+        infostr = f'{len(found_cats)} {len(found_products)} {bcolors.GREEN}+({len(found_products - existing_products)}){bcolors.ENDC}'
+        print(infostr, flush=True, end='\r')
+        time.sleep(0.1)
+    except (KeyboardInterrupt, NoSuchWindowException):
         break
 browser.quit()
+
+
+with db_ctx() as db:
+    for url in found_products:
+        db.execute(
+            text('INSERT INTO product (url) VALUES (:url) ON CONFLICT DO NOTHING'),
+            dict(url=url)
+        )
+    db.commit()
