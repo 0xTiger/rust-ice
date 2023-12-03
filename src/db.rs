c783221677c7cd11cce3cba5889fb03f9221be96
0xTiger <tgh.lizard@googlemail.com>
2023-12-03 14:07:27 +0000

    fix: gtin is nullable

--- src/db.rs
+++ src/db.rs
@@ -9,7 +9,7 @@ use std::env;
 #[derive(sqlx::FromRow)]
 #[derive(Serialize)]
 pub struct Product {
-    pub gtin: i32,
+    pub gtin: Option<i32>,
     pub name: String,
     pub sku: i64,
     pub image: String,
